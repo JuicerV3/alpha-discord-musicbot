01e98232c8ef3167c49351aa20eb1b0b3f2ed222
JuicerV3 <wl.juicer@gmail.com>
2024-04-22 20:04:19 +0700

    typo

--- commands/music/play.js
+++ commands/music/play.js
@@ -19,7 +19,7 @@ module.exports = {
 			return interaction.reply('You are not in a voice channel.');
 		const query = interaction.options.getString('query');
 
-		// check for spotify podcast because discord-player throw error
+		// except for spotify podcast because discord-player throw error
 		if (query.includes('open.spotify.com/episode')) {
 			return interaction.reply(
 				'Spotify podcast not supported at the moment.'
