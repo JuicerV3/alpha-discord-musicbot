74334bf822fc08d0b1dd291bea46e72e2136a2ef
JuicerV3 <wl.juicer@gmail.com>
2024-04-26 18:07:53 +0700

    Typo: Uppercase K

--- commands/music/play.js
+++ commands/music/play.js
@@ -73,7 +73,7 @@ module.exports = {
 			function nFormatter(num, digits) {
 				const lookup = [
 					{ value: 1, symbol: '' },
-					{ value: 1e3, symbol: 'k' },
+					{ value: 1e3, symbol: 'K' },
 					{ value: 1e6, symbol: 'M' },
 					{ value: 1e9, symbol: 'G' },
 					{ value: 1e12, symbol: 'T' },
