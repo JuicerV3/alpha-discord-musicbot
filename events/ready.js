ee8d4591ffa3067857830d190e2e124abb5888c9
JuicerV3 <wl.juicer@gmail.com>
2024-04-27 21:53:19 +0700

    Add streaming presence on start

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,4 @@
-const { Events } = require('discord.js');
+const { Events, ActivityType } = require('discord.js');
 // console.log when bot ready.
 module.exports = {
 	name: Events.ClientReady,
@@ -8,5 +8,15 @@ module.exports = {
 			`\u001b[1;32mConnection established. Ready! Logged in as ${client.user.tag}` +
 				'\u001b[0m'
 		);
+		client.user.setPresence({
+			activities: [
+				{
+					name: 'Music',
+					type: ActivityType.Streaming,
+					url: 'https://twitch.tv/music',
+				},
+			],
+			status: 'online',
+		});
 	},
 };
