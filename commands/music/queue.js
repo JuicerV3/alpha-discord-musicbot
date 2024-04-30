bf437970a83a6cbfa26955de0c2b9e7167b5222c
JuicerV3 <wl.juicer@gmail.com>
2024-04-30 09:23:31 +0700

    Added queue clear command

--- commands/music/queue.js
+++ commands/music/queue.js
@@ -1,6 +1,9 @@
 const { SlashCommandBuilder, EmbedBuilder, Options } = require('discord.js');
 const { useQueue } = require('discord-player');
-const { sourceFormatter, loopStatusFormatter } = require('../../functions/formatter');
+const {
+	sourceFormatter,
+	loopStatusFormatter,
+} = require('../../functions/formatter');
 
 module.exports = {
 	category: 'music',
@@ -11,11 +14,17 @@ module.exports = {
 			subcommand.setName('list').setDescription('list current queue')
 		)
 		.addSubcommand((subcommand) =>
+			subcommand.setName('clear').setDescription('Clear queue')
+		)
+		.addSubcommand((subcommand) =>
 			subcommand
 				.setName('remove')
 				.setDescription('Remove song from queue')
 				.addStringOption((option) =>
-					option.setName('index').setDescription('Song index number').setRequired(true)
+					option
+						.setName('index')
+						.setDescription('Song index number')
+						.setRequired(true)
 				)
 		),
 	async execute(interaction) {
@@ -40,7 +49,9 @@ module.exports = {
 				(track, index) =>
 					`${++index}. [${track.title}](${track.url})\n └─ @${
 						track.requestedBy.username
-					} • ${sourceFormatter(queue.currentTrack.source)} • ${track.duration}`
+					} • ${sourceFormatter(queue.currentTrack.source)} • ${
+						track.duration
+					}`
 			);
 
 			let trackQueue;
@@ -66,22 +77,63 @@ module.exports = {
 						queue.currentTrack.url
 					})**\nRequested by @${
 						queue.currentTrack.requestedBy.username
-					} • ${sourceFormatter(queue.currentTrack.source, queue.currentTrack.views)} • ${
-						queue.currentTrack.duration
-					}
+					} • ${sourceFormatter(
+						queue.currentTrack.source,
+						queue.currentTrack.views
+					)} • ${queue.currentTrack.duration}
 				`
 				)
 				.setFields({ name: 'Tracklist', value: trackQueue })
 				.setFooter({
-					text: `${queue.durationFormatted} • Loop status: ${loopStatusFormatter(
-						queue.repeatMode
-					)}`,
+					text: `${
+						queue.durationFormatted
+					} • Loop status: ${loopStatusFormatter(queue.repeatMode)}`,
 				});
 			const msg = await interaction.editReply({ embeds: [embed] });
 			return setTimeout(() => msg.delete(), 30000);
+		} else if (interaction.options.getSubcommand() === 'clear') {
+			const tracks = queue.tracks.map(
+				(track, index) => `${++index}. ${track.title}`
+			);
+
+			if (tracks.length < 1) {
+				const embed = new EmbedBuilder()
+					.setColor(0xfffa6b)
+					.setTitle('Cannot remove song')
+					.setDescription('There is no song in the current queue');
+				const msg = await interaction.editReply({ embeds: [embed] });
+				return setTimeout(() => msg.delete(), 10000);
+			}
+
+			try {
+				queue.clear();
+
+				const embed = new EmbedBuilder()
+					.setColor(0x3ec489)
+					.setAuthor({
+						name: interaction.user.username,
+						iconURL: interaction.user.avatarURL(),
+					})
+					.setTitle('Queue cleared');
+				const msg = await interaction.editReply({ embeds: [embed] });
+				return setTimeout(() => msg.delete(), 15000);
+			} catch (e) {
+				console.error(e);
+
+				const embed = new EmbedBuilder()
+					.setColor(0xfffa6b)
+					.setTitle('Something went wrong')
+					.setDescription(
+						`Something went wrong while trying to clear the queue \`${query}\``
+					);
+				const msg = await interaction.editReply({ embeds: [embed] });
+				return setTimeout(() => msg.delete(), 10000);
+			}
 		} else if (interaction.options.getSubcommand() === 'remove') {
 			const songIndex = interaction.options.getString('index');
-			const tracks = queue.tracks.map((track, index) => `${++index}. ${track.title}`);
+			const tracks = queue.tracks.map(
+				(track, index) => `${++index}. ${track.title}`
+			);
 
 			if (tracks.length < 1) {
 				const embed = new EmbedBuilder()
@@ -117,7 +169,9 @@ module.exports = {
 						iconURL: interaction.user.avatarURL(),
 					})
 					.setTitle('Song removed')
-					.setDescription(`Removed ${tracks[songIndex - 1]} from the queue`);
+					.setDescription(
+						`Removed ${tracks[songIndex - 1]} from the queue`
+					);
 				const msg = await interaction.editReply({ embeds: [embed] });
 				return setTimeout(() => msg.delete(), 15000);
 			} catch (e) {
