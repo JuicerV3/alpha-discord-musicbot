ce44f09b04c7bf79ec39a4650a91bc5184facaeb
JuicerV3 <wl.juicer@gmail.com>
2024-05-04 20:33:18 +0700

    Initial help command

---
+++ commands/utility/help.js
@@ -0,0 +1,30 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+
+module.exports = {
+	category: 'utility',
+	data: new SlashCommandBuilder()
+		.setName('ping')
+		.setDescription('Replies with pong!'),
+	async execute(interaction) {
+		await interaction.deferReply();
+		const embed = new EmbedBuilder()
+			.setColor(0x96ffff)
+			.setAuthor({
+				name: interaction.client.user.username,
+				iconURL: interaction.client.user.avatarURL(),
+			})
+			.setTitle('List of commands')
+			.setFooter({
+				text: `Code with pain by @_juicerv3`,
+				iconURL: interaction.client.user.avatarURL(),
+			})
+			.addFields({
+				name: 'Commands',
+				value: `${interaction.client.ws.ping}ms`,
+				inline: true,
+			})
+			.setTimestamp();
+		const msg = await interaction.editReply({ embeds: [embed] });
+		return setTimeout(() => msg.delete(), 30000);
+	},
+};
