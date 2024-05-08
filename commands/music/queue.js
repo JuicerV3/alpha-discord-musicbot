6181ebdf025da12204483520084116ee6b74ff06
JuicerV3 <wl.juicer@gmail.com>
2024-05-08 18:32:01 +0700

    Add queue jump command

--- commands/music/queue.js
+++ commands/music/queue.js
@@ -18,6 +18,17 @@ module.exports = {
 		)
 		.addSubcommand((subcommand) =>
 			subcommand
+				.setName('jump')
+				.setDescription('jump to specific track')
+				.addStringOption((option) =>
+					option
+						.setName('index')
+						.setDescription('Track index number')
+						.setRequired(true)
+				)
+		)
+		.addSubcommand((subcommand) =>
+			subcommand
 				.setName('remove')
 				.setDescription('Remove track from the queue')
 				.addStringOption((option) =>
@@ -116,6 +127,65 @@ module.exports = {
 					.setTitle('Something went wrong')
 					.setDescription(
 						`Something went wrong while trying to clear the queue \`${query}\``
+					);
+				const msg = await interaction.editReply({ embeds: [embed] });
+				return setTimeout(() => msg.delete(), 10000);
+			}
+		} else if (interaction.options.getSubcommand() === 'jump') {
+			const songIndex = interaction.options.getString('index');
+			const tracks = queue.tracks.map(
+				(track, index) => `${++index}. ${track.title}`
+			);
+
+			let trackQueue;
+			if (tracks.length < 1) {
+				trackQueue = 'Queue is empty';
+			} else if (tracks.length > 9) {
+				tracksQueue = tracks.slice(0, 10).join('\n');
+				tracksQueue += `\nand ${tracks.length - 10} other songs`;
+			} else {
+				trackQueue = tracks.join('\n');
+			}
+
+			if (tracks.length < 1) {
+				const embed = new EmbedBuilder()
+					.setColor(0xfffa6b)
+					.setTitle('There is no track in the current queue');
+				const msg = await interaction.editReply({ embeds: [embed] });
+				return setTimeout(() => msg.delete(), 10000);
+			}
+
+			if (tracks[songIndex - 1] === undefined) {
+				const embed = new EmbedBuilder()
+					.setColor(0xfffa6b)
+					.setTitle('Cannot jump the queue')
+					.setDescription(
+						`Track number **${songIndex}** does not exist.\nMake sure to choose correct song index`
+					)
+					.setFields({
+						name: 'Tracklist',
+						value: `${trackQueue}`,
+					});
+				const msg = await interaction.editReply({ embeds: [embed] });
+				return setTimeout(() => msg.delete(), 20000);
+			}
+
+			try {
+				queue.node.jump(songIndex - 1);
+
+				const embed = new EmbedBuilder()
+					.setColor(0x96ffff)
+					.setTitle(`Jumped to ${tracks[songIndex - 1]}`);
+				const msg = await interaction.editReply({ embeds: [embed] });
+				return setTimeout(() => msg.delete(), 15000);
+			} catch (e) {
+				console.error(e);
+
+				const embed = new EmbedBuilder()
+					.setColor(0xf54242)
+					.setTitle('Something went wrong')
+					.setDescription(
+						`Something went wrong while trying to jump to the song \`${query}\``
 					);
 				const msg = await interaction.editReply({ embeds: [embed] });
 				return setTimeout(() => msg.delete(), 10000);
