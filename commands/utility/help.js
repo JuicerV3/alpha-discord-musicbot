34bfb564f2f9c4f36a011d7d890589412374e943
JuicerV3 <wl.juicer@gmail.com>
2024-05-09 22:50:32 +0700

    Add music commands list

--- commands/utility/help.js
+++ commands/utility/help.js
@@ -14,14 +14,14 @@ module.exports = {
 				iconURL: interaction.client.user.avatarURL(),
 			})
 			.setTitle('List of commands')
+			.addFields({
+				name: 'Music commands',
+				value: `\`/play\` Play a song\n\`/pause\` Pause current song\n\`/resume\` Resume current song\n\`/back\` Back to previous song\n\`/skip\` Skip to next song\n\`/seek\` Seek current song\n\`/loop\` Set player loop mode\n\`/nowplaying\` Show nowplaying song\n\`/queue list\` list current queue\n\`/queue jump\` jump to song on the queue\n\`/queue remove\` Remove song from the queue\n\`/queue clear\` Clear all song from the queue\n\`/radio\` Play DASHRADIO Stream\n\`/stop\` Stop the player\n\`/disconnect\` Disconnect bot from the voice channel`,
+				inline: true,
+			})
 			.setFooter({
 				text: `Code with pain by @_juicerv3`,
 				iconURL: interaction.client.user.avatarURL(),
-			})
-			.addFields({
-				name: 'Commands',
-				value: `${interaction.client.ws.ping}ms`,
-				inline: true,
 			})
 			.setTimestamp();
 		const msg = await interaction.editReply({ embeds: [embed] });
